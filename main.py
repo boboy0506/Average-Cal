041a799bcfb6990c916e71e98a793524e2e50704
booobooyyy <hm0ejd74@gmail.com>
2023-06-05 08:55:13 +0800

    added average meter

--- main.py
+++ main.py
@@ -8,8 +8,18 @@ def firstsem():
     philo = int(input("Introduction to Philosophy                    : "))
     pe    = int(input("Physical Education                            : "))
     average = [oral, scie, etech, math, ucsp, eapp, philo, pe] 
-    total = (oral + scie + etech + math + ucsp + eapp + philo + pe) /8
-    print(total)
+    total = round(oral + scie + etech + math + ucsp + eapp + philo + pe) /8
+    print(f"Your General Average: {total}")
+    if total > 94:
+        print("With High Honor")
+    elif total >89 and total <94:
+        print("With Honor")
+    elif total >84 and total <89:
+        print("Achiever")
+    elif total > 74 and total <84:
+        print("You Passed")
+    else:
+        print("You Failed")
     highest = max(average)
     lowest = min(average)
     print(f"Your Highest grade in this sem is {highest}")
@@ -24,8 +34,25 @@ def secondsem():
     stats    = int(input("Statistics and Probability                 : "))
     research = int(input("Practical Research                         : "))
     java     = int(input("JAVA                                       : "))
+    average = [century, scie, rr, stats, java, research, pe] 
+    total = round(century + scie +  rr + stats + java + research + pe) /7
+    print(f"Your General Average: {total}")
+    if total > 94:
+        print("With High Honor")
+    elif total >89 and total <94:
+        print("With Honor")
+    elif total >84 and total <89:
+        print("Achiever")
+    elif total > 74 and total <84:
+        print("You Passed")
+    else:
+        print("You Failed")
+    highest = max(average)
+    lowest = min(average)
+    print(f"Your Highest grade in this sem is {highest}")
+    print(f"Your Lowest grade in this sem is {lowest}")
 
-print("First sem  [1]\nSecond sem [2]\nWhole sem [3]")
+print("First sem  [1]\nSecond sem [2]")
 
 while True:
     user = int(input("Enter a sem: "))
@@ -36,7 +63,4 @@ if user == 1:
     firstsem()
 if user == 2:
     secondsem()
-if user == 3:
-    firstsem()
-    secondsem()
-    
+
